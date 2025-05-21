e6946ab0a4f329aa92c3bee3a9bac0502844cc24
budarov <a.budarov@haulmont.com>
2025-05-21 14:55:55 +0400

    example report: Recently added book items

---
+++ reports-sample/src/main/java/com/company/library/reports/RecentlyAddedBookItemsReport.java
@@ -0,0 +1,59 @@
+package com.company.library.reports;
+
+import com.company.library.reports.annotation.*;
+import io.jmix.reports.entity.DataSetType;
+import io.jmix.reports.entity.Orientation;
+import io.jmix.reports.entity.ParameterType;
+import io.jmix.reports.entity.ReportOutputType;
+
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@ReportDef(
+        name = "Recently added book items",
+        code = "RECENTLY_ADDED_BOOK_ITEMS",
+        group = DemoReportGroup.class
+)
+public interface RecentlyAddedBookItemsReport {
+
+    @InputParameterDef(
+            alias = "createDt",
+            name = "Create After",
+            type = ParameterType.DATETIME,
+            required = true
+    )
+    void createDtInputParameter();
+
+    @InputParameterTransformation(alias = "createDt")
+    default Object createDtTransform(@ParameterValue Date paramValue) {
+        return paramValue.toInstant().atOffset(ZoneOffset.UTC);
+    }
+
+    @BandDef(name = BandDef.ROOT, root = true, orientation = Orientation.HORIZONTAL)
+    void rootBand();
+
+    @BandDef(name = "headerBookInstances", parent = BandDef.ROOT, orientation = Orientation.HORIZONTAL)
+    void headerBookInstancesBand();
+
+    @BandDef(name = "BookInstances", parent = BandDef.ROOT, orientation = Orientation.HORIZONTAL)
+    @DataSetDef(
+            type = DataSetType.JPQL,
+            query = """
+                    select bookPublication_book.name as "bookPublication.book.name",
+                    libraryDepartment.name as "libraryDepartment.name"
+                    from BookInstance e
+                    left join e.bookPublication.book bookPublication_book
+                    left join e.libraryDepartment libraryDepartment
+                    where e.createdDate >= ${createDt}
+                    """
+    )
+    void bookInstancesBand();
+
+    @TemplateDef(
+            outputType = ReportOutputType.XLSX,
+            filePath = "com/company/library/reports/new/RecentlyAddedBookItems.xlsx",
+            isDefault = true,
+            outputNamePattern = "Recently added book items.xlsx"
+    )
+    void defaultTemplate();
+}
